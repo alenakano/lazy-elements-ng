a920dee3f87da92ba228d7ea30e662dea3899b91
Alexandre <alex.nakano@gmail.com>
2020-02-15 20:10:26 -0300

    Correcao do server do element.

--- foo/server/server.js
+++ foo/server/server.js
@@ -2,5 +2,5 @@ var express = require('express');
 var path = require('path');
 
 var app = express();
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use('/element', express.static(__dirname + '/dist'));
 app.listen(3000, () => console.log('Rodando na porta 3000'));
